8898097bba071c1834f4db3c2888ab7c682bc085
Kevin Whitley <krwhitley@gmail.com>
2022-10-24 14:17:26 -0500

    released v0.2.4 - removed itty, LLC from footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,7 +40,7 @@
 
   <Footer constrained={constrained}>
     <div class="split">
-      &copy; {year} itty, LLC. All rights reserved.
+      &copy; {year} Kevin R. Whitley. All rights reserved.
 
       <div class="social">
         <a href="https://twitter.com/kevinrwhitley">
